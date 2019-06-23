2cd4ac80f3a04f36f34b9638ba2e6985a50400c9
KDKHOST <marcelobradrj@gmail.com>
2019-06-22 21:13:14 -0300

    Update index.php

--- includes/namespace/index.php
+++ includes/namespace/index.php
@@ -1,5 +1,8 @@
 <?php
     require_once("config.php");
 
-    
+    $cad = new Cadastro();
+    $cad->setNome("Marcelo");
+    $cad->setEmail("contato@kdkhost.com.br");
+    $cad->setSenha("admin123.");
 ?>
