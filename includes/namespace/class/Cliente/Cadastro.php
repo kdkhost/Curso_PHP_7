c3cbca8366c8a7c753b1355a7f4511d739288e49
KDKHOST <marcelobradrj@gmail.com>
2019-06-22 22:22:57 -0300

    up

--- includes/namespace/class/Cliente/Cadastro.php
+++ includes/namespace/class/Cliente/Cadastro.php
@@ -2,7 +2,7 @@
     namespace Cliente;
     class Cadastrar extends \Cadastrar {
         public function registrarVendas(){
-            echo "Foi Registrado uma venda para o cliente ".$this->getName();
+            echo "Foi Registrado uma venda para o cliente ".$this->getNome();
         }
     }
 ?>
