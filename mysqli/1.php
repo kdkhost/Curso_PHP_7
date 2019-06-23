b7c96121e468f68abd98cfc86fc9be4ab9d1934b
KDKHOST <marcelobradrj@gmail.com>
2019-06-23 20:03:47 -0300

    atualizando sistema
    #1 curso de PHP 7 Udemy

--- mysqli/1.php
+++ mysqli/1.php
@@ -6,4 +6,15 @@ if ($conn->connect_error){
     echo "Error: " .$conn->connect_error;
 }
 
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUE(?, ?)");
+$stmt->bind_param("ss", $deslogin, $dessenha);
+
+$deslogin = "admin";
+$dessenha = "12345";
+$stmt->execute();
+
+$deslogin = "root";
+$dessenha = "67890";
+$stmt->execute();
+
 ?>
