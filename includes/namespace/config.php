c378d1d5df158c1aaf11431114560e42607e2ebc
KDKHOST <marcelobradrj@gmail.com>
2019-06-22 21:10:05 -0300

    criando projeto

---
+++ includes/namespace/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    spl_autoload_register(function($nameClasse){
+        $dirClass = "class";
+        $filename = $dirClass . DIRECTORY_SEPARATOR . $nameClasse . ".php";
+
+        if (file_exists($filename)){
+            require_once($filename);
+        }
+    })
+
+?>
