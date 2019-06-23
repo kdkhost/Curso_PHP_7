55254bace4628fe78c5e324eace1e635dbe285d1
KDKHOST <marcelobradrj@gmail.com>
2019-06-22 22:01:58 -0300

    troca de nome
    de Cadastro para Registro

--- includes/namespace/index.php
+++ includes/namespace/index.php
@@ -1,7 +1,7 @@
 <?php
     require_once("config.php");
 
-    $cad = new Cadastro();
+    $cad = new Registro();
     $cad->setNome("Marcelo");
     $cad->setEmail("contato@kdkhost.com.br");
     $cad->setSenha("admin123.");
