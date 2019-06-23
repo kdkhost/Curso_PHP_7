f3f764224c1306c02eb178ed79d39dfd4c90d7ed
KDKHOST <marcelobradrj@gmail.com>
2019-06-23 17:25:17 -0300

    anotações

--- comandos_sql.php
+++ comandos_sql.php
@@ -1,1 +1,38 @@
-<
+<?php
+/*
+Comandos para criar tabela no banco de dados
+*/
+
+echo "<div>
+CREATE TABLE tb_usuarios (
+    idusuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    deslogin VARCHAR(64) NOT NULL,
+    dessenha VARCHAR(256) NOT NULL,
+    dtcadastro TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP()
+);
+</div>"
+
+/*
+comando para inserir um cadastro na tabela criada
+*/
+
+echo "<div>
+INSERT INTO tb_usuarios (deslogin, dessenha) VALUES('eduardo','123456');
+</div>"
+
+/*
+comando para atualizar/alterar um cadastro na tabela criada
+*/
+
+echo "<div> UPDATE tb_usuarios SET dessenha = '7891012' WHERE idusuario = 1;
+</div>"
+
+/*
+comando para Deletar um cadastro na tabela criada
+*/
+
+echo "<div>
+DELET FROM tb_usuarios WHERE idusuario = 1;
+</div>"
+
+?>
