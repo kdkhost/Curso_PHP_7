41f02ef17b48fc3d93cf4f26a5d29854ba553504
KDKHOST <marcelobradrj@gmail.com>
2019-06-25 09:33:26 -0300

    update

--- includes/namespace/config.php
+++ includes/namespace/config.php
@@ -5,7 +5,8 @@
         var_dump($nameClasse);
 
         $dirClass = "class";
-        $filename = $dirClass . DIRECTORY_SEPARATOR . $nameClasse . ".php";
+        //$filename = $dirClass . DIRECTORY_SEPARATOR . $nameClasse . ".php";
+        $filename = str_replace ("\\", "/",$dirClass . DIRECTORY_SEPARATOR . $nameClass . ".php");
 
         if (file_exists($filename)){
             require_once($filename);
