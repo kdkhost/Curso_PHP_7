323580a95096df56bde6fd71bb0e419949e4a0d2
KDKHOST <marcelobradrj@gmail.com>
2019-06-25 13:31:22 -0300

    Create sql.php

--- includes/DAO/sql.php
+++ includes/DAO/sql.php
@@ -10,7 +10,7 @@
 
        private function setParams($statment, $parameters = array()){
         foreach($parameters as $key => $value){
-            $this -> setParam($key, $value);
+            $this->setParam($key, $value);
         }
        }
 
